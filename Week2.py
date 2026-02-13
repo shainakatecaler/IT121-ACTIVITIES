7594edbc164ee66e62ed1d190cdc110104bd27d9
shainakatecaler <cshainakate@gmail.com>
2026-02-13 08:37:49 +0800

    Create Week2.py

---
+++ Week2.py
@@ -0,0 +1,16 @@
+fullname = "Shaina Kate Plaza Caler"
+age = 25
+height = 1.54
+is_working = False
+
+print("Fullname:", fullname)
+print(type(fullname))
+
+print("\nAge:", age)
+print(type(age))
+
+print("\nHeight in meters:", height)
+print(type(height))
+
+print("\nCurrently working?", is_working)
+print(type(is_working))
